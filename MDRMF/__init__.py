c2d7d5899357879fe69c2b2113f2f51b932ca828
Jacob Molin <jacob.molin@me.com>
2023-05-23 13:36:59 +0200

    changes

--- MDRMF/__init__.py
+++ MDRMF/__init__.py
@@ -1,6 +1,6 @@
 # Import modules
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 from .featurizer import Featurizer
 from .moleculeloader import MoleculeLoader
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='MDRMF',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     description='Multidrug Resistance Machine Fishing',
     author='Jacob Molin Nielsen',
-    author_email='xsj110@sund.ku.dk',
+    author_email='jacob.molin@me.com',
     url='https://github.com/MolinDiscovery/MDRMF',  # use the URL to the github repo
-    download_url='https://github.com/MolinDiscovery/MDRMF/archive/0.1.tar.gz',  # I'll explain this in a second
-    keywords=['some', 'keywords'],  # arbitrary keywords
+    download_url='https://github.com/MolinDiscovery/MDRMF/archive/v0.0.2.tar.gz',  # I'll explain this in a second
+    keywords=['machine fishing', 'drug discovery', 'machine learning', 'pool based active learning'],  # arbitrary keywords
     classifiers=[],
 )
