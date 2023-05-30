22eced86835ac4914f8244ceabe64e768ccf160c
Jacob Molin <jacob.molin@me.com>
2023-05-30 12:18:42 +0200

    changes

--- MDRMF/models/rfmodeller.py
+++ MDRMF/models/rfmodeller.py
@@ -5,8 +5,8 @@ from ..dataset import Dataset
 
 class RFModeller(Modeller):
 
-    def __init__(self, dataset, iterations=10, initial_sample_size=10, acquisition_size=10, acquisition_method="greedy", retrain=True, **kwargs) -> None:
-        super().__init__(dataset, iterations, initial_sample_size, acquisition_size, acquisition_method, retrain)
+    def __init__(self, dataset, evaluator=None, iterations=10, initial_sample_size=10, acquisition_size=10, acquisition_method="greedy", retrain=True, **kwargs) -> None:
+        super().__init__(dataset, evaluator, iterations, initial_sample_size, acquisition_size, acquisition_method, retrain)
         from sklearn.ensemble import RandomForestRegressor
         self.kwargs = kwargs
         self.model = RandomForestRegressor(**self.kwargs)
@@ -32,15 +32,20 @@ class RFModeller(Modeller):
                 model_dataset = self.dataset.merge_datasets([model_dataset, acquired_pts])
 
             if self.retrain:
-                # Retrain with new model_dataset
+                # Reset model and train
                 self.model = RandomForestRegressor(**self.kwargs)
                 self.model.fit(model_dataset.X, model_dataset.y)
             else:
                 # Train on existing model
                 self.model.fit(model_dataset.X, model_dataset.y)
 
+            if self.evaluator is not None:
+                self.call_evaluator(i=i)
 
-        return self.model #model_dataset
+        # If we return the amount of molecules trained on along with a set of predictions, the experimenter can use this info
+        # to make desired k-100 graphs. Although this should probably be handled by an evaluator class. We call the evaluator class and get
+        # info that we want to evaluate on each iteration. This could also be R^2 for the range of values in the model_dataset and other other things.
+        return self.model
     
     def predict(self, dataset: Dataset):
 
