1ce6edb82500356efe153cfdbf4d9c68143743e9
Jacob Molin <jacob.molin@me.com>
2023-10-31 20:16:22 +0100

    change

--- MDRMF/models/__init__.py
+++ MDRMF/models/__init__.py
@@ -2,3 +2,8 @@
 
 from .modeller import Modeller
 from .rfmodeller import RFModeller
+from .dtmodeller import DecisionTreeRegModeller
+from .knnmodeller import KNNRegModeller
+from .lgbmmodeller import LGBMModeller
+from .mlprmodeller import MLPRModeller
+from .svrmodeller import SVRModeller
