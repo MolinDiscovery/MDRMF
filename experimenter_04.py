f22760daebf498b6603ad77c809a4d003f18e5b3
MolinDiscovery <jacob.molin@me.com>
2024-08-29 07:45:45 +0000

    Update files

--- experimenter_04.py
+++ experimenter_04.py
@@ -3,5 +3,5 @@
 from MDRMF.experimenter import Experimenter
 from MDRMF.dataset import Dataset
 
-exp = Experimenter("experiment_setups/03-article/04-LGBM-desc-pair-10k.yaml")
+exp = Experimenter("experiment_setups/03-article/pairwise10k/06-MLP-desc-pair-10k.yaml")
 exp.conduct_all_experiments()
